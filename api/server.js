4abebe0cb38dcb9aa731c83e19b06c8e9c955085
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-03-07 14:47:26 -0600

    Working on endpoints

--- api/server.js
+++ api/server.js
@@ -1,5 +1,23 @@
-// BUILD YOUR SERVER HERE
+const Users = require("./users/model");
 const express = require("express");
 const server = express();
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+//this invokation returns something that tells our server to read our data as JSON
+
+server.get("/api/users", (req, res) => {
+  //pull users from db
+  //send users back to client
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "something bad happened.",
+        error: err.message,
+      });
+    });
+});
+
+module.exports = server;
