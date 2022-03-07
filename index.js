8a138711c0b39b1bd8ebc3af4f09a9b0ec8c7dd3
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-03-07 14:31:39 -0600

    Initialized the server

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require("express");
-const server = express();
-const port = 9000;
+const server = require("./api/server");
 
 server.listen(5000, () => {
-  console.log(`server listening on ${port}`);
+  console.log(`server listening on port 5000`);
 });
